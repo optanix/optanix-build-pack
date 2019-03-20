e4a6bf693ef0aa57d92693d00756184c783cea3a
John Lude <John.Lude@optanix.com>
2019-03-20 12:59:05 +0000

    perl.sh edited online with Bitbucket
    Adding slash

--- lib/perl.sh
+++ lib/perl.sh
@@ -9,13 +9,13 @@ run_build() {
   if [ -d $build_dir/src ]; then
 
     puts_step "Moving src/lib"
-    copy_and_rm_dir "${build_dir}/src/lib" "${build_root}/usr/local/lib/perl5"
+    copy_and_rm_dir "${build_dir}/src/lib/" "${build_root}/usr/local/lib/perl5"
 
     puts_step "Moving src/bin"
-    copy_and_rm_dir "${build_dir}/src/bin" "${build_root}/usr/local/bin"
+    copy_and_rm_dir "${build_dir}/src/bin/" "${build_root}/usr/local/bin"
 
     puts_step "Moving src/cgi-bin"
-    copy_and_rm_dir "${build_dir}/src/cgi-bin" "${build_root}/var/www/cgi-bin"
+    copy_and_rm_dir "${build_dir}/src/cgi-bin/" "${build_root}/var/www/cgi-bin"
   fi
 }
 
